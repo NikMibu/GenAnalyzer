62c41f7f21cc05d1d16d329ce8e28ba54dab444c
NikMibu <niklasmibuu@gmail.com>
2025-04-09 21:55:15 +0200

    create main branch

--- src/main.cpp
+++ src/main.cpp
@@ -1,7 +1,18 @@
 #include <iostream>
 
 int main() {
-    std::cout << "GenAnalyzer läuft!" << std::endl;
-    std::cout << "Setup funktioniert auf beiden PCs" << std::endl;
-    return 0;
+    try
+    {
+        std::cout << "🧬 Willkommen bei GenAnalyzer!" << std::endl;
+
+        // Schritt 1: Genome einlesen
+        // Schritt 2: Disease-Liste einlesen
+        // Schritt 3: Analyse starten
+        return 0;
+    }
+    catch(const std::exception& e)
+    {
+        std::cerr << e.what() << '\n';
+        return -1;
+    }
 }
